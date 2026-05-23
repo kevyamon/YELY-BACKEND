2fa1a202ad932f41453aea2db54d8e0b018821b0
baticlean <baticlean225@gmail.com>
2026-05-23 17:57:37 +0000

    correction redirection et cover 3

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -436,7 +436,7 @@ const renderShareHtml = async (res, seller, userAgent = '') => {
 <body>
   <div class="container">
     <div class="avatar-wrapper">
-      <img class="logo-img" src="${seller.profilePicture || 'https://res.cloudinary.com/' + cloudName + '/image/upload/v1/yely/assets/yely_default_storefront'}" alt="Boutique" />
+      <img class="logo-img" src="${seller.profilePicture || 'https://download-yely.vercel.app/logo.png'}" alt="Boutique" />
       <div class="badge-icon">
         <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M23 12L20.56 9.22L20.9 5.54L17.29 4.72L15 1.4L11.5 2.92L8 1.4L5.71 4.72L2.1 5.54L2.44 9.22L0 12L2.44 14.78L2.1 18.46L5.71 19.28L8 22.6L11.5 21.08L15 22.6L17.29 19.28L20.9 18.46L20.56 14.78L23 12ZM10 17L6 13L7.41 11.59L10 14.17L16.59 7.58L18 9L10 17Z" fill="#D4AF37"/>
