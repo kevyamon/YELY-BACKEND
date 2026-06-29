3ce9dd4d6eacf8f49ebd7f1c2c03c35ad4b84dcf
baticlean <baticlean225@gmail.com>
2026-06-29 01:36:09 +0000

    Peaufinage2

--- src/routes/rideRoutes.js
+++ src/routes/rideRoutes.js
@@ -14,16 +14,17 @@ const {
 
 // LECTURE COURSE EN COURS (La route qui manquait)
 router.get('/current', protect, authorize('rider', 'driver', 'seller'), rideController.getCurrentRide);
+router.get('/estimate', protect, authorize('rider', 'seller', 'superadmin'), rideController.estimateRide);
+router.get('/history', protect, rideController.getRideHistory);
+
 router.get('/:id', protect, authorize('rider', 'driver', 'seller', 'superadmin'), rideController.getRideById);
 
 // ROUTES DE LECTURE & ANNULATION ET RATING
-router.get('/estimate', protect, authorize('rider', 'seller', 'superadmin'), rideController.estimateRide);
 router.post('/emergency-cancel', protect, authorize('rider', 'seller', 'driver', 'superadmin'), rideController.emergencyCancel);
 router.put('/:id/cancel', protect, authorize('rider', 'driver', 'seller', 'superadmin'), rideController.cancelRide);
 router.put('/:id/rate', protect, authorize('rider', 'seller', 'superadmin'), rideController.rateRide);
 
 // HISTORIQUE
-router.get('/history', protect, rideController.getRideHistory);
 router.delete('/:id/history', protect, rideController.hideFromHistory);
 
 // ROUTES D'ACTION AVEC VALIDATION STRICTE DU BODY
